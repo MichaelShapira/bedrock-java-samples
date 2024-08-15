6701d91ec3ddc3628935b0f8a8f43149deae0e2c
conderol <michshap@amazon.com>
2024-08-15 11:02:03 +0300

    Init

---
+++ src/main/java/aws/example/Main.java
@@ -0,0 +1,25 @@
+package aws.example;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        String modelId = "anthropic.claude-3-haiku-20240307-v1:0";
+        String prompt = "Provide details about beluga. Provide a lot of details and format your answer in JSON format";
+        BedrockHelper.invokeModel(modelId, prompt);
+
+        BedrockHelper.invokeModelWithStream(modelId, prompt);
+        
+        BedrockHelper.converseApi(modelId,"/dummy_full_path_to_image");
+
+        BedrockHelper.invokeAgent("Some prompt",
+                                 "some agent id",
+                                 "some agent alias id",
+                                 "some session id");
+        
+        System.out.println("\n");
+    }
+}
